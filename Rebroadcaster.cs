b8bacaa5a8e98970c16d2b8509ed25e8585ed533
Jesse Govindan <jesse@cybicom.com>
2021-02-01 19:34:13 +0200

    Add manual project start up

---
+++ Rebroadcaster.cs
@@ -0,0 +1,22 @@
+﻿using Rebroadcaster.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Rebroadcaster
+{
+    class Rebroadcaster : Application
+    {
+        [STAThread]
+        public static void Main(String[] args)
+        {
+            var app = new Rebroadcaster();
+            app.MainWindow = new MainWindowView();
+            app.MainWindow.Show();
+            app.Run();
+        }
+    }
+}
